839a18142d96ceee0f07dfddf6d6b86a53ff0202
suseecho <suseecho@sina.com>
2016-10-18 13:08:25 +0800

    Delete dpj.py

--- dpj.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3.5
-# -*- coding: utf-8 -*-
-import os
-import sys
-args_1 = sys.argv[1]
-os.rename(args_1,"test.c")
-os.system("sdcc test.c << packihx > test.hex |  hex2bin test.hex | sudo stcflash test.bin") 
